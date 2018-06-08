ad12e8025530da1b860c23b493cacd1ec9e63f8e
StefanKwinta <38467124+StefanKwinta@users.noreply.github.com>
2018-06-08 17:38:11 +0200

    Some dialogues and argorythmes

--- Assets/scripts/DialogueHendler.cs
+++ Assets/scripts/DialogueHendler.cs
@@ -10,12 +10,20 @@ public class DialogueHendler : MonoBehaviour {
     public Text DialogueOption2;
     public Text DialogueOption3;
     public Text DialogueOption4;
-    private DialoguePart actualDialogue;
+    public Button DialogueButton1;
+    public Button DialogueButton2;
+    public Button DialogueButton3;
+    public Button DialogueButton4;
+    private DialoguePart actualDialogue = null;
 
     public void ChoisedOption1()
-    {   
-        if(actualDialogue.TextDialogues[0] != null && actualDialogue.TextDialogues[0].NPCTextPart != null)
-            NPCText.GetComponent<Text>().text = actualDialogue.TextDialogues[0].NPCTextPart;
+    {
+        if (actualDialogue.TextDialogues[0] != null && actualDialogue.TextDialogues[0].NPCTextPart != null)
+        {
+            actualDialogue = actualDialogue.TextDialogues[0];
+            NPCText.GetComponent<Text>().text = actualDialogue.NPCTextPart;
+            updateButtons();
+        }
         else
             NPCText.GetComponent<Text>().text = "Koniec rozmowy";
     }
@@ -45,18 +53,65 @@ public class DialogueHendler : MonoBehaviour {
     
     void Start()
     {
-        string[] buttons = { "Łoddawaj passssata złodzjeju!!!","Somsiad, jo ci dam somsiada łajdaku!","Teraz sopbie pogadamy, nie masz gdzie uciec.","Wrr (popraw skarpety w sandałach)" };
-        DialoguePart[] dialogs = { new DialoguePart(), new DialoguePart(), new DialoguePart(), new DialoguePart() };
-        actualDialogue = new DialoguePart("Ło nie, tuż to somsiad!",buttons,dialogs); 
-        NPCText.text = actualDialogue.NPCTextPart;
-        DialogueOption1.text = actualDialogue.Answsers[0];
-        DialogueOption2.text = actualDialogue.Answsers[1];
-        DialogueOption3.text = actualDialogue.Answsers[2];
-        DialogueOption4.text = actualDialogue.Answsers[3];
+        actualDialogue = setDialogue(); 
     }
     // Update is called once per frame
     void Update () {
-		
+        if (!DialogueButton1.enabled && !DialogueButton2.enabled && !DialogueButton3.enabled && !DialogueButton4.enabled)
+            Debug.Log("Koniec Gadania");
 	}
+
+    private DialoguePart setDialogue()
+    {
+        DialoguePart result;
+        string[] buttons = { "Łoddawaj passssata złodzjeju!!!", "Somsiad, jo ci dam somsiada łajdaku!", "Teraz sopbie pogadamy, nie masz gdzie uciec.", "Wrr (popraw skarpety w sandałach)" };
+        DialoguePart[] dialogs = {
+            new DialoguePart(),
+            new DialoguePart(),
+            new DialoguePart(),
+            new DialoguePart() };
+        dialogs[0].NPCTextPart = "A więc tak, w takim razie niech ta cienciwa zaspiewa tango.";
+        result = new DialoguePart("Ło nie, tuż to somsiad!", buttons, dialogs);
+        NPCText.text = result.NPCTextPart;
+        DialogueOption1.text = result.Answsers[0];
+        DialogueOption2.text = result.Answsers[1];
+        DialogueOption3.text = result.Answsers[2];
+        DialogueOption4.text = result.Answsers[3];
+        return result;
+    }
+
+    private void updateButtons()
+    {
+        if (actualDialogue.Answsers.Length == 4)
+        {
+            if (actualDialogue.Answsers[0] != null)
+                DialogueOption1.text = actualDialogue.Answsers[0];
+            else { 
+                DialogueButton1.enabled = false;
+                DialogueOption1.text = "";
+            }
+            if (actualDialogue.Answsers[1] != null)
+                DialogueOption2.text = actualDialogue.Answsers[1];
+            else
+            {
+                DialogueButton2.enabled = false;
+                DialogueOption2.text = "";
+            }
+            if (actualDialogue.Answsers[2] != null)
+                DialogueOption3.text = actualDialogue.Answsers[2];
+            else
+            {
+                DialogueButton3.enabled = false;
+                DialogueOption3.text = "";
+            }
+            if (actualDialogue.Answsers[3] != null)
+                DialogueOption4.text = actualDialogue.Answsers[3];
+            else
+            {
+                DialogueButton4.enabled = false;
+                DialogueOption4.text = "";
+            }
+        }
+    }
     
 }
