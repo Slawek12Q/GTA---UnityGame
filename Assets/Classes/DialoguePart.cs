a4c002d15b1b062a8bee801e77694d969601c8cc
StefanKwinta <38467124+StefanKwinta@users.noreply.github.com>
2018-06-08 13:59:51 +0200

    Dialogue
    Dialogue

---
+++ Assets/Classes/DialoguePart.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Classes
+{
+    class DialoguePart
+    {
+        public string NPCTextPart { get; set; }
+        public string[] Answsers = new string[4];
+        public DialoguePart[] AextDialogues = new DialoguePart[4];
+        DialoguePart()
+        {
+
+        }
+        DialoguePart(string npctext,string[] answers,DialoguePart[]nextDialogues)
+        {
+
+        }
+    }
+}
---
+++ Assets/scripts/DialogueHendler.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueHendler : MonoBehaviour {
+
+    public Text NPCText;
+    public Button DialogueOption1;
+    public Button DialogueOption2;
+    public Button DialogueOption3;
+    public Button DialogueOption4;
+
+
+    public void ChoisedOption1()
+    {
+        NPCText.GetComponent<Text>().text = "First option";
+    }
+    public void ChoisedOption2()
+    {
+        NPCText.GetComponent<Text>().text = "second option";
+    }
+    public void ChoisedOption3()
+    {
+        NPCText.GetComponent<Text>().text = "third option";
+    }
+    public void ChoisedOption4()
+    {
+        NPCText.GetComponent<Text>().text = "last option";
+    }
+
+    void Start()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
