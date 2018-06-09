66a9cb66a1020c13f41341a2508e5fc51275af28
KamalinPal <bartodziejkamil@gmail.com>
2018-06-09 15:13:46 +0200

    Upgraded bullet
    It destorys enemys now

--- Assets/scripts/Bullet.cs
+++ Assets/scripts/Bullet.cs
@@ -20,6 +20,14 @@ public class Bullet : MonoBehaviour
 		
 	}
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag=="Enemy")
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+
     private void OnBecameInvisible()
     {
         Destroy(gameObject);
