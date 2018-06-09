16b83983a970843715ec1ecbbc657075a54fdcab
KamalinPal <bartodziejkamil@gmail.com>
2018-06-09 10:21:42 +0200

    Enemy changed

--- Assets/scripts/Enemy.cs
+++ Assets/scripts/Enemy.cs
@@ -13,27 +13,21 @@ public class Enemy : MonoBehaviour
 	void Start ()
     {
         rigidbody=GetComponent<Rigidbody2D>();
-        rigidbody.velocity=Vector3.down*speed;
+        rigidbody.velocity=transform.right*speed;
 	}
 
     // Update is called once per frame
     void Update ()
     {
-        //var player = GameObject.FindGameObjectWithTag("Player");
-        //transform.LookAt(player.transform.position);
-        //transform.position+=transform.forward*speed*Time.deltaTime;
+        Vector2 current = transform.position;
+        var direction = GameObject.FindGameObjectWithTag("Player").transform.position;
+        var angle = Mathf.Atan2(direction.y, direction.x)*Mathf.Rad2Deg;
+        transform.rotation=Quaternion.AngleAxis(angle, Vector3.forward);
+        rigidbody.velocity=transform.right*speed;
     }
 
     private void OnBecameInvisible()
     {
         Destroy(gameObject);
     }
-
-    //public void LookAt2D(this Transform transform, Vector2 target)
-    //{
-    //    Vector2 current = transform.position;
-    //    var direction = target-current;
-    //    var angle = Mathf.Atan2(direction.y, direction.x)*Mathf.Rad2Deg;
-    //    transform.rotation=Quaternion.AngleAxis(angle, Vector3.forward);
-    //}
 }
