2d069e1209179e7a55d4e3a58c2da2de28047308
StefanKwinta <38467124+StefanKwinta@users.noreply.github.com>
2018-06-09 12:50:03 +0200

    Dialogue finished v1

--- Assets/scripts/DialogueHendler.cs
+++ Assets/scripts/DialogueHendler.cs
@@ -81,7 +81,16 @@ public class DialogueHendler : MonoBehaviour
         DialoguePart result;
         string[] buttons = { "Łoddawaj passssata złodzjeju!!!", "Somsiad, jo ci dam somsiada łajdaku!", "Teraz sopbie pogadamy, nie masz gdzie uciec.", "Wrr (popraw skarpety w sandałach)" };
         string[] butonsSecondPart = { "Passat to nie wszystko somsiedzie.", "Może i tak ale n ciebie nawet bez pasata jestem z mocny!", "Łoż ty złodzjeju, jo ci dom kradzejstwo!", "Ty mi tu nie opwiadaj kocmołuchów, bo Grarzynka bigosu nagotowała i stygnie!" };
-        DialoguePart[] secondDialoguePart = { new DialoguePart(), new DialoguePart(), new DialoguePart(), new DialoguePart() };
+        string[] butonsFinalPart = { "Somsiad to jednak swój chłop!", "To może jeszce po maluszku na zgodę.. HEHE.", "Nie poznaje somsida, tuz to szlachetne zachowanie.", "(Odbierz kluczyki w milczeniu)......" };
+        DialoguePart[] finalDialoguePart = { new DialoguePart(), new DialoguePart(), new DialoguePart(), new DialoguePart() };
+        finalDialoguePart[0].NPCTextPart = "Trzymaj się!";
+        finalDialoguePart[1].NPCTextPart = "Halinka by mnie z chałupy wywaliła, mówiła że ostarni raz to wytrzymuje takie zachowanie także w tym tygodniu to lipa z piciem. Może kiedy indziej, narzie spadam.";
+        finalDialoguePart[2].NPCTextPart = "Każdy ma swoje ideały somsiedzie, żegnaj.";
+        finalDialoguePart[3].NPCTextPart = "........................";
+        DialoguePart[] secondDialoguePart = { new DialoguePart(), new DialoguePart(), new DialoguePart(), new DialoguePart("A więć nie jesteś taki za jakiego cię wziołem... stawieasz bigos nad materialne korzysci, dobrze więc proszę oto kluczyki do twojego passata.", butonsFinalPart, finalDialoguePart) };
+        secondDialoguePart[0].NPCTextPart = "Ale wszystko bez passata to nic. Mam dosyć tych twojch gierek!";
+        secondDialoguePart[1].NPCTextPart = "DOŚĆ! To twój koniec!";
+        secondDialoguePart[2].NPCTextPart = "Niech zadecyduje próba walki, zobaczymy kto jest prawdziwym złodzjejem!";
         DialoguePart[] dialogs = {
             new DialoguePart(),
             new DialoguePart("No co mi zrobisz, bez passata nie masz żadnej mocy... BUAHAHAHAHAHA!",butonsSecondPart,secondDialoguePart),
