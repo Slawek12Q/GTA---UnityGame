50253bee6ae41618dfb208a54ddd77e5e2cf3e5e
Slawek12Q <slawek.guzik@interia.pl>
2018-06-09 13:06:08 +0200

    BulletDamage

---
+++ Assets/scripts/BulletDamage.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collision2D))]
+public class BulletDamage : MonoBehaviour {
+
+    public int Damage = 1;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter2D (Collision2D collision)
+    {
+        var health = collision.gameObject.GetComponent<HP>();
+
+        if (health != null)
+            health.HP_character -= 1;
+
+        Destroy(gameObject);
+    }
+}
