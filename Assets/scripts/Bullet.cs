c284f6fad1ec19c7ad3cb3fc801a5de663838ef3
Slawek12Q <slawek.guzik@interia.pl>
2018-06-09 17:45:34 +0200

    EnemyHp

--- Assets/scripts/Bullet.cs
+++ Assets/scripts/Bullet.cs
@@ -20,13 +20,7 @@ public class Bullet : MonoBehaviour
 		
 	}
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if(collision.gameObject.tag=="Enemy")
-        {
-            Destroy(collision.gameObject);
-        }
-    }
+   
 
     private void OnBecameInvisible()
     {
