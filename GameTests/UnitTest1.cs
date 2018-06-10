e2cb90b931a2178d472c3e8cd9440230597310fe
StefanKwinta <38467124+StefanKwinta@users.noreply.github.com>
2018-06-10 14:54:35 +0200

    TestsEmptyAdded

---
+++ GameTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using System;
+using Assets.Classes;
+
+namespace GameTests
+{
+    [TestFixture]
+    public class DialogueTests
+    {
+        [Test]
+        public void notNull()
+        {
+            new DialoguePart();
+        }
+    }
+}
